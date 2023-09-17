1010ca00362c99a07629b0a88adae59de23f4410
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2023-09-17 18:07:46 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.04 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String solution(String X, String Y) {
+        String answer = "";
+        int[] x = new int[10];
+        int[] y = new int[10];
+        
+        StringBuilder sb = new StringBuilder(); 
+        for(char c : X.toCharArray()){
+            x[c-'0']++;
+        }
+        for(char c : Y.toCharArray()){
+            y[c-'0']++;
+        }
+        
+        for(int i=9; i>=0; i--){
+            if(x[i] > 0 && y[i] > 0) {
+                int cnt = Math.min(x[i], y[i]);
+                //System.out.println(i+","+cnt); 
+                
+                for(int j=0; j<cnt; j++){
+                    sb.append(i);  
+                }
+            }
+        }
+        answer = sb.toString();
+        
+        if(answer.equals("")) return "-1";
+        else if(answer.replace("0","") == "") answer = "0";
+
+        return answer; 
+    }
+}
