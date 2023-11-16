663b8c04f9e43fcb3a98d02ca0c082d5d583cf1c
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2023-11-16 17:30:50 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 84.69 ms, Memory: 95 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -0,0 +1,23 @@
+import java.util.*; 
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(String s : participant){
+            map.put(s, map.getOrDefault(s, 0) + 1);     
+        }
+        
+        for(String s : completion){
+            if(map.containsKey(s)) map.put(s, map.get(s)-1); 
+            if(map.get(s) == 0) map.remove(s); 
+        }
+        
+        for(String key : map.keySet()){
+            answer = key; 
+        }
+        
+        return answer;
+    }
+}
