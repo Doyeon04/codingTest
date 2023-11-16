7aae1a6a878e5c24748c2983b1984dc2e2368ec4
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2023-11-16 18:19:10 +0900

    [level 1] Title: 문자열 나누기, Time: 0.51 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.java
@@ -1,23 +1,23 @@
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        char first = s.charAt(0); 
+        char first = 0; 
         int firstCnt = 0, elseCnt = 0; 
         
         
-        for(int i=0; i<s.length(); i++){
-            char c = s.charAt(i); 
-            
-            if(c==first) firstCnt++;
-            else elseCnt++; 
+        for(char c : s.toCharArray()){
+            if(first == 0) first = c;
+            if(c == first) firstCnt++; 
+            else elseCnt++;
             
             if(firstCnt == elseCnt){
-                answer++;
-                if(i < s.length()-1)  first = s.charAt(i+1);
-            }else if(i==s.length()-1) answer++; 
-         
+                firstCnt = elseCnt = first = 0;
+                answer++; 
+            }
+        
         }
-
+        if(first != 0) answer++; 
+        
         return answer;
     }
 }
