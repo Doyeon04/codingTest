273709d0a9e20cb92af369886e9c753cbc54196b
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2023-11-16 13:47:23 +0900

    [level 1] Title: 최소직사각형, Time: 1.93 ms, Memory: 92.9 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,6 +1,14 @@
 class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
+        
+        int a = 0, b = 0; 
+        
+        for(int[] card : sizes){
+            a = Math.max(a, Math.max(card[0], card[1]));
+            b = Math.max(b, Math.min(card[0], card[1])); 
+        }
+        answer = a * b; 
         return answer;
     }
 }
