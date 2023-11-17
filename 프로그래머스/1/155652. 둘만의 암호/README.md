2dcefa234df2ea207d8fb87b66503735960f936e
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2023-11-17 12:16:16 +0900

    [level 1] Title: 둘만의 암호, Time: 0.38 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/README.md
+++ 프로그래머스/1/155652. 둘만의 암호/README.md
@@ -1,10 +1,10 @@
 # [level 1] 둘만의 암호 - 155652 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155652#) 
 
 ### 성능 요약
 
-메모리: 82.4 MB, 시간: 12.78 ms
+메모리: 82.5 MB, 시간: 0.38 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 5일 11:47:11
+2023년 11월 5일 12:16:14
 
 ### 문제 설명
 
--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,24 +1,25 @@
 class Solution {
     public String solution(String s, String skip, int index) {
-        String answer = "";
-        
-        for(char alpha : s.toCharArray()){
+        StringBuilder answer = new StringBuilder();
+
+        for (char alpha : s.toCharArray()) {
             char newAlpha = alpha;
-         
-            for(int i=0; i<index; i++){
-                newAlpha++; 
-                
-                if(newAlpha > 'z') newAlpha-=26; 
-                
-                 if(skip.contains(String.valueOf(newAlpha))){ 
-                      i--;
-                  }
+
+            for (int i = 0; i < index; i++) {
+                char nextAlpha = (char) ((newAlpha - 'a' + 1) % 26 + 'a');
+
+                while (skip.indexOf(nextAlpha) != -1) {
+                    nextAlpha = (char) ((nextAlpha - 'a' + 1) % 26 + 'a');
+                }
+
+        
+                if (Character.isAlphabetic(newAlpha)) {
+                    newAlpha = nextAlpha;
+                }
             }
-            answer+=newAlpha; 
+            answer.append(newAlpha);
         }
-        return answer;
+
+        return answer.toString();
     }
 }
-
-
-
