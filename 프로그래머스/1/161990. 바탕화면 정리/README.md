6fc9e27628756952b2bfdbef360ac9d8cba1b413
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2023-11-20 19:47:28 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.16 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/README.md
+++ 프로그래머스/1/161990. 바탕화면 정리/README.md
@@ -1,10 +1,10 @@
 # [level 1] 바탕화면 정리 - 161990 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161990) 
 
 ### 성능 요약
 
-메모리: 72.9 MB, 시간: 0.26 ms
+메모리: 74.7 MB, 시간: 0.16 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 1일 19:33:15
+2023년 11월 1일 19:47:26
 
 ### 문제 설명
 
--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,27 +1,17 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
-        int[] answer = new int[4];
-        char[][] arr = new char[wallpaper.length][wallpaper[0].length()];
+        int minCol = 51, minRow = 51, maxCol = 0, maxRow = 0; 
         
-        int minCol = 50, minRow = 50, maxCol = 0, maxRow = 0; 
-        
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr[0].length; j++){
-                arr[i][j] = wallpaper[i].charAt(j);
-                if(arr[i][j]=='.') continue; 
-                minCol = Math.min(minCol, i);
-                minRow = Math.min(minRow, j);
-                maxCol = Math.max(maxCol, i);
-                maxRow = Math.max(maxRow, j); 
+        for(int i=0; i<wallpaper.length; i++){
+            for(int j=0; j<wallpaper[i].length(); j++){
+                if(wallpaper[i].charAt(j)=='.') continue;
+                minRow = Math.min(minRow, i);
+                minCol = Math.min(minCol, j);
+                maxRow = Math.max(maxRow, i);
+                maxCol = Math.max(maxCol, j); 
             }
         }
- 
-        answer[0] = minCol;
-        answer[1] = minRow;
-        answer[2] = maxCol+1;
-        answer[3] = maxRow+1; 
-        return answer;
+        
+        return new int[]{minRow, minCol, maxRow+1, maxCol+1};
     }
 }
-
-
