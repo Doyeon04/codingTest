c2d86552a2c61de975a125c00315ab86a8882769
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2023-11-30 17:38:24 +0900

    [Silver III] Title: Java vs C++, Time: 156 ms, Memory: 16204 KB -BaekjoonHub

--- 백준/Silver/3613. Java vs C＋＋/Java vs C＋＋.java
+++ 백준/Silver/3613. Java vs C＋＋/Java vs C＋＋.java
@@ -8,30 +8,25 @@ public class Main {
         String ans="";
 
         if(Character.isUpperCase(str.charAt(0)) // 대문자로 시작하거나 시작이나 끝이 _ 라면 에러
-        || str.startsWith("_") || str.endsWith("_")){
+        || str.startsWith("_") || str.endsWith("_") || str.indexOf("__") > 0){
             System.out.println("Error!");
             return;
         }
 
-        boolean error = false;
         if(str.contains("_")){
-            if(str.indexOf("__") > 0) error =true;
             boolean isUnder = false;
             for(char c : str.toCharArray()){
                 if(Character.isUpperCase(c)){  // _ 와 대문자가 같이 나오면 에러
-                    error= true;
-                    break;
+                    System.out.println("Error!");
+                    return;
                 }
                 if(isUnder) ans+=Character.toUpperCase(c);
                 else if(c!='_') ans+=c;
 
-                if(c=='_') {
-                    isUnder=true;
-                }
+                if(c=='_') isUnder=true;
                 else isUnder=false;
             }
         }else{
-
             for(char c : str.toCharArray()){
                 if(Character.isUpperCase(c)){
                     ans = ans + "_" + Character.toLowerCase(c);
@@ -39,7 +34,7 @@ public class Main {
             }
         }
 
-        System.out.println(error ? "Error!" : ans);
+        System.out.println( ans);
     }
 }
 
