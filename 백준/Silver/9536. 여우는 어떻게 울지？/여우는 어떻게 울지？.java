041ba5029025618c446dbb649ca60b81bf5fdc65
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2023-12-01 11:12:27 +0900

    [Silver III] Title: 여우는 어떻게 울지?, Time: 120 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Silver/9536. 여우는 어떻게 울지？/여우는 어떻게 울지？.java
+++ 백준/Silver/9536. 여우는 어떻게 울지？/여우는 어떻게 울지？.java
@@ -1,26 +1,27 @@
 import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         while (T-- > 0) {
             String[] sounds = br.readLine().split(" ");
+            Set<String> set = new HashSet<>();
             while (true) {
                 String str = br.readLine();
                 if (str.equals("what does the fox say?")) break;
                 String animalSound = str.split(" ")[2];
-                for (int i = 0; i < sounds.length; i++) {
-                    if (sounds[i].equals(animalSound)) sounds[i] = "";
-                }
-
+                set.add(animalSound);
             }
-            String ans = "";
             for (String s : sounds) {
-                if (!s.equals("")) ans += s + " ";
+                if(!set.contains(s)) sb.append(s).append(" "); 
             }
-            System.out.println(ans);
+            sb.append("\n");
         }
+        System.out.println(sb);
     }
 }
 
