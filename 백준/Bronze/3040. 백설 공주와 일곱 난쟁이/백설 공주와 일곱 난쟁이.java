fe0ec7a301b17a59a1bad98cd0d2b4dbca33e4ea
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2023-12-20 19:36:23 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 124 ms, Memory: 14096 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -6,26 +6,22 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] arr = new int[9];
-        int sum=0;
-        for(int i=0; i<9; i++){
-            int num = Integer.parseInt(br.readLine());
-            arr[i] = num;
-            sum+=num;
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
         }
-        boolean stop = false;
-        for(int i=0; i<9; i++){
-            for(int j=i+1; j<9; j++){
-                if(sum-arr[i]-arr[j] == 100){
-                    arr[i]=0;
-                    arr[j]=0;
-                    stop = true;
-                    break;
+        int a = 0, b = 0;
+        for (int i = 0; i < 9; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - arr[i] - arr[j] == 100) {
+                    a = i;
+                    b = j;
                 }
             }
-            if(stop) break; 
         }
-        for (int x : arr) {
-            if(x!=0) System.out.println(x);
+        for (int i = 0; i < arr.length; i++) {
+            if (i != a && i != b) System.out.println(arr[i]);
         }
     }
 }
