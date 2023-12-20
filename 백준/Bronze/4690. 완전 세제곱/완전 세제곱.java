f9152e6b081636f20322af5ee8b400c016562a93
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2023-12-20 17:57:58 +0900

    [Bronze III] Title: 완전 세제곱, Time: 1756 ms, Memory: 16392 KB -BaekjoonHub

---
+++ 백준/Bronze/4690. 완전 세제곱/완전 세제곱.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 100000000
+        for (int a = 2; a <= 100; a++) {
+            for (int b = 2; b <= 100; b++) {
+                for (int c = b + 1; c <= 100; c++) {
+                    for (int d = c + 1; d <= 100; d++) {
+                        if (Math.pow(a, 3) == Math.pow(b, 3) + Math.pow(c, 3) + Math.pow(d, 3)) {
+                            System.out.println("Cube = " + a + ", Triple = (" + b + "," + c + "," + d + ")");
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
+
+
