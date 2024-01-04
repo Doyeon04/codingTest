bf4a7e9fef1b283ef884209425736c378b6f20b4
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2024-01-04 11:14:48 +0900

    [Silver II] Title: 유기농 배추, Time: 156 ms, Memory: 15936 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,56 +1,62 @@
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main{
-    static int n, m, k;
-    static int[][] arr;
+public class Main {
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static void dfs(int y, int x){
+    static int n, m, k;
+    static int[][] map;
 
-        for(int i=0; i<4; i++){
-            int nx = x+dx[i];
-            int ny = y+dy[i];
-
-            if(nx<0 || nx>=m || ny<0 || ny>=n || arr[ny][nx]==0) continue;
-
-            arr[ny][nx] = 0;
-            dfs(ny, nx);
-
-        }
-
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        while(T-->0){
-            st = new StringTokenizer(br.readLine());
+
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             m = Integer.parseInt(st.nextToken());
             n = Integer.parseInt(st.nextToken());
             k = Integer.parseInt(st.nextToken());
-            arr = new int[n][m];
-
-            while(k-->0){
+            map = new int[n][m];
+            while (k-- > 0) {
                 st = new StringTokenizer(br.readLine());
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
-                arr[y][x] = 1;
+                map[y][x] = 1;
             }
             int cnt = 0;
-            for(int i=0; i<n; i++){
-                for(int j=0; j<m; j++){
-                    if(arr[i][j] == 1){
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (map[i][j] == 1) {
                         cnt++;
-                        arr[i][j] =0;
-                        dfs(i, j);
+                        bfs(i, j);
                     }
                 }
             }
             System.out.println(cnt);
+        }
+
+    }
+
+    static void bfs(int y, int x){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{x, y});
+        map[y][x] = 0;
+
+        while(!q.isEmpty()){
+            int[] poll = q.poll();
+
+            for(int i=0; i<4; i++){
+                int nx = poll[0] + dx[i];
+                int ny = poll[1] + dy[i];
+                if(nx < 0 || nx >= m || ny < 0 || ny >= n || map[ny][nx]==0) continue;
+
+                q.offer(new int[]{nx, ny});
+                map[ny][nx] = 0;
+            }
         }
     }
 }
