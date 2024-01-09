1474df60e753fba0121e0998a25fa126c4428f56
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2024-01-09 18:34:50 +0900

    [Silver II] Title: 점프 점프, Time: 272 ms, Memory: 24876 KB -BaekjoonHub

--- 백준/Silver/14248. 점프 점프/점프 점프.java
+++ 백준/Silver/14248. 점프 점프/점프 점프.java
@@ -7,41 +7,30 @@ public class Main {
     static int n, s;
     static int[] rocks;
     static boolean[] visit;
+    static int cnt;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        rocks = new int[n+1];
-        visit = new boolean[n+1];
+        rocks = new int[n + 1];
+        visit = new boolean[n + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=1; i<=n; i++){
+        for (int i = 1; i <= n; i++) {
             rocks[i] = Integer.parseInt(st.nextToken());
         }
         s = Integer.parseInt(br.readLine());
-
-        visit[s] = true;
         dfs(s);
-
-        int cnt=0;
-        for (boolean b : visit) {
-            if(b) cnt++;
-        }
         System.out.println(cnt);
 
     }
-    static void dfs(int s){
 
-        int right = s + rocks[s];
-        int left = s - rocks[s];
+    static void dfs(int s) {
+        if (s < 1 || s > n || visit[s]) return;
 
-        if(right >= 1 && right <= n){
-            dfs(right);
-            visit[right] = true;
-        }
-
-        if(left >= 1 && left <= n){
-            dfs(left);
-            visit[left] = true;
-        }
+        visit[s] = true;
+        cnt++;
+        dfs(s + rocks[s]);
+        dfs(s - rocks[s]);
     }
 }
 
