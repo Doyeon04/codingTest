eb121c764a85f939fed2fdb3b5239838ee46c1d8
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2024-01-15 09:50:40 +0900

    [Silver V] Title: 폴리오미노, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.java
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -8,33 +8,12 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String board = br.readLine();
 
-        int cnt=0;
-        String ans = "";
-        boolean flag = true;
-        int idx=0;
-        for(char c : board.toCharArray()){
-            idx++;
-            if(c == '.' || idx == board.length()) {
-                if(c!='.') cnt++;
-                int cntA = cnt / 4;
-                cnt = cnt % 4;
-                int cntB=0;
-                if(cnt % 2==0) cntB = cnt / 2;
-                else flag = false;
-                
-                for(int i=0; i<cntA; i++){
-                    ans += "AAAA";
-                }
-                for(int i=0; i<cntB; i++){
-                    ans += "BB";
-                }
-                if(c=='.') ans+='.';
-                cnt=0;
-            }else cnt++;
-            
-        }
-        if(!flag) System.out.println(-1);
-        else System.out.println(ans);
+        board = board.replaceAll("XXXX", "AAAA");
+        board = board.replaceAll("XX", "BB");
+
+        if(board.contains("X")){
+            System.out.println(-1);
+        }else System.out.println(board);
     }
 }
 
