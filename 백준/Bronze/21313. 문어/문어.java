d23ac9b36196da34ce19ec6b1e73576f28153793
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2024-01-16 11:33:08 +0900

    [Bronze II] Title: 문어, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/21313. 문어/문어.java
+++ 백준/Bronze/21313. 문어/문어.java
@@ -9,37 +9,23 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         int[][] arr = new int[n + 1][9];
-        
+        StringBuilder sb = new StringBuilder();
+
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= 8; j++) {
-                if (i == n) { // 마지막 문어라면
-                    if(arr[i][j]==0 && arr[1][j]==0){  // 나와 그다음 문어의 사용하지 않은 손이라면
-                       arr[i][j]=1; // 내 손 사용
-                       arr[1][j]=1;  // 그 다음 문어도 같은 손 사용
-                        System.out.println(j);
-                        break;
-                    }
-                } else {
-                    if(arr[i][j]==0 && arr[i+1][j] == 0){
-                        arr[i][j] = 1;
-                        arr[i+1][j] = 1;
-                        System.out.println(j);
-                        break;
-                    }
+                int next = i + 1;
+                if (i == n) next = 1;
+
+                if (arr[i][j] == 0 && arr[next][j] == 0) {
+                    arr[i][j] = 1; // 내손 사용
+                    arr[next][j] = 1; // 그다음 문어도 같은 손 사용
+                    sb.append(j).append(" ");
+                    break;
                 }
             }
         }
-        
-        /*
-        1 - 2 : 1 (1번손)
-        2 - 3 : 2 (2번손)
-        3 - 4 : 3 (1번손)
-        4 - 5 : 4 (2번손)
-        5 - 1 : 5 (3번손?)
-         */
-
-
+        System.out.println(sb);
     }
 }
 
