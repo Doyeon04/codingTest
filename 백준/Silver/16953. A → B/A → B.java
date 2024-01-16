bd392a2b899ea85213cd8598251c8b9d3385d229
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2024-01-16 10:21:29 +0900

    [Silver II] Title: A → B, Time: 128 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/16953. A → B/A → B.java
+++ 백준/Silver/16953. A → B/A → B.java
@@ -4,37 +4,32 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    static int a, b;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        a = Integer.parseInt(st.nextToken());
-        b = Integer.parseInt(st.nextToken());
-
-        bfs(a);
-    }
-
-    static void bfs(int v){
-        Queue<long[]> q = new LinkedList<>();
-        q.offer(new long[]{v, 1});
-
-        while(!q.isEmpty()){
-            long[] poll = q.poll();
-            long n = poll[0];
-            long cnt = poll[1];
-
-            if(n==b){
-                System.out.println(cnt);
-                return;
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+        while (true) {
+            if (b % 10 == 1) {
+                b /= 10;
+                cnt++;
+            } else if(b % 2 == 0){
+                b /= 2;
+                cnt++;
+            }else{
+                System.out.println(-1);
+                break;
             }
-
-            if(n*2 <= b) q.add(new long[]{n*2, cnt+1});
-            if(n*10+1 <= b) q.add(new long[]{n*10 + 1, cnt+1});
-
+            if (b == a) {
+                System.out.println(cnt + 1);
+                break;
+            }else if (b < a) {
+                System.out.println(-1);
+                break;
+            }
         }
-        System.out.println(-1);
     }
 
 }
+
