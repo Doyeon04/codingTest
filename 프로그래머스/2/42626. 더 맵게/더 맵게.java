22c9504fcdff296841cfa707c1b6adbf327d1768
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2024-02-09 16:26:07 +0900

    [level 2] Title: 더 맵게, Time: 3686.98 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i=0; i<scoville.length; i++){
+            pq.add(scoville[i]); 
+        }
+    
+        while(pq.size()>1){
+            int food1 = pq.poll();
+            int food2 = pq.poll();
+          
+            if(food1 >= K && food2 >= K){
+                break;
+            }
+            
+            answer++; 
+            
+            int mixed = food1 + food2 * 2; 
+            pq.add(mixed); 
+            
+        }
+        
+        if(pq.size()==1 && pq.poll() < K) answer = -1;  
+        
+        return answer;
+    }
+}
