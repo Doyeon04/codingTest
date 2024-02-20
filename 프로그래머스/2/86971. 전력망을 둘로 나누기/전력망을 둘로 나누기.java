86abbc76c2e9e845ece80aa5b823679be36bc0f3
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2024-02-20 10:53:32 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.80 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -7,50 +7,35 @@ class Solution {
         int answer = Integer.MAX_VALUE;
         arr = new int[n+1][n+1];
         visit = new boolean[n+1]; 
-        
+
         for(int i=0; i<wires.length; i++){
             int a = wires[i][0];
             int b = wires[i][1];
             arr[a][b] = arr[b][a] = 1; 
         }
-        boolean[][] disconnected = new boolean[n+1][n+1]; 
-        
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                if(arr[i][j] == 1 && !disconnected[i][j]){ // 끊어진적 없는 곳이라면
-                    arr[i][j] = arr[j][i] = 0;
-                    disconnected[i][j] = disconnected[j][i] = true; 
-                    visit = new boolean[n+1]; 
-                    answer = Math.min(answer, count(arr, n)); 
-                    arr[i][j] = arr[j][i] =  1; // 다시 이어줌
-                }
-            }
-        }
-         return answer; 
-    
-    }
-    static int count(int[][] arr, int n){
-      
-        List<Integer> list = new ArrayList<>(); 
-        for(int i=1; i<=n; i++){
-            if(!visit[i]) {
-                cnt=0; 
-                dfs(i, n);
-                list.add(cnt); 
-            }
-        }
 
-        if(list.size()==2){
-            return Math.abs(list.get(0) - list.get(1)); 
-        }else return Integer.MAX_VALUE; 
+        for(int i=0; i<wires.length; i++){
+            int a = wires[i][0];
+            int b = wires[i][1];
+            
+            arr[a][b] = arr[b][a] = 0; // 선 끊기
+            
+            cnt=0;
+            visit = new boolean[n+1];
+            dfs(arr, a, n);
+            answer = Math.min(answer, Math.abs(cnt - (n-cnt))); 
+
+            arr[a][b] = arr[b][a] =  1; // 다시 이어줌
+        }
+        return answer; 
+
     }
-    
-    static void dfs(int v, int n){
+    static void dfs(int[][] arr, int v, int n){
         visit[v] = true; 
         cnt++; 
         for(int i=1; i<=n; i++){
             if(arr[v][i]==1 && !visit[i]){
-                dfs(i, n);
+                dfs(arr, i, n);
             }
         }
     }
