a74e37a41460c868d0b572c0a01e600d3ba2ad26
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2024-03-15 11:54:11 +0900

    [Silver II] Title: 영재의 시험, Time: 1504 ms, Memory: 300568 KB -BaekjoonHub

--- 백준/Silver/19949. 영재의 시험/영재의 시험.java
+++ 백준/Silver/19949. 영재의 시험/영재의 시험.java
@@ -18,16 +18,17 @@ public class Main {
     }
     static void dfs(int len){
         if(len == 10){
-            String s = "";
+            StringBuilder sb = new StringBuilder();
             for (int num : nums) {
-                s+=num;
+                sb.append(num);
             }
+            String s = sb.toString();
             if(s.contains("111")) return;
             if(s.contains("222")) return;
             if(s.contains("333")) return;
             if(s.contains("444")) return;
             if(s.contains("555")) return;
-            
+
 
             int score=0;
             for(int i=0; i<10; i++){
