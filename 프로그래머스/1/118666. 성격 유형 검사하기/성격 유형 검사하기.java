ddc1795d5d362cedc375a833f898323a448bf791
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2024-03-25 21:37:31 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 2.43 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,38 +1,59 @@
 import java.util.*;
 
+class Conversion{
+    boolean agree;
+    int score; 
+    public Conversion(boolean agree, int score){
+        this.agree = agree; 
+        this.score = score; 
+    }
+}
+
+
 class Solution {
     public String solution(String[] survey, int[] choices) {
         String answer = "";
-        
-        String[][] types = {{"R", "T"}, {"C", "F"}, {"J", "M"}, {"A", "N"}};
-        int[] score = {3, 2, 1, 0, 1, 2, 3};
-        Map<String, Integer> map = new HashMap<>();
+      
+        Conversion[] results = new Conversion[8];
+        results[1] = new Conversion(false, 3);
+        results[2] = new Conversion(false, 2);
+        results[3] = new Conversion(false, 1);
+        results[4] = new Conversion(false, 0);
+        results[5] = new Conversion(true, 1);
+        results[6] = new Conversion(true, 2);
+        results[7] = new Conversion(true, 3);
+       
+        HashMap<String, Integer> map = new HashMap<>(); // 문자, 점수 
         
         for(int i=0; i<survey.length; i++){
-            String type = ""; 
-            if(choices[i] < 4){ // 비동의
-                 type = survey[i].substring(0, 1); 
-            }else if(choices[i] >4){ // 동의
-                 type = survey[i].substring(1); 
+           String alpha = survey[i]; 
+            if(results[choices[i]].agree){ // 동의면 오른쪽
+               alpha = alpha.substring(1);  
+            }else{ // 비동의면 왼쪽
+               alpha = alpha.substring(0, 1);  
             }
-            int addScore = score[choices[i]-1]; 
-            map.put(type, map.getOrDefault(type, 0) + addScore); 
+            map.put(alpha, map.getOrDefault(alpha, 0) + results[choices[i]].score); 
         }
-        for(String[] type : types){
-            int leftScore =0; 
-            int rightScore =0; 
+  
+        String[][] typeArr = {{"R", "T"}, {"C", "F"}, {"J", "M"}, {"A", "N"}}; 
+        for(int i=0; i<4; i++){
+            int leftScore = 0;
+            int rightScore = 0;
+            String left = typeArr[i][0];
+            String right = typeArr[i][1]; 
+       
+            if(map.containsKey(left)) leftScore = map.get(left);
+            if(map.containsKey(right)) rightScore = map.get(right);
             
-            if(map.containsKey(type[0])) leftScore=map.get(type[0]); 
-            if(map.containsKey(type[1])) rightScore=map.get(type[1]);  
-            
-            if(leftScore > rightScore) answer+=type[0]; 
-            else if(leftScore < rightScore) answer+=type[1]; 
-            else {
-                if(type[0].compareTo(type[1])<0) answer+=type[0];
-                else answer+=type[1]; 
+            if(leftScore > rightScore) answer+=left;
+            else if(leftScore < rightScore) answer+=right;
+            else{ // 같으면 사전순으로 빠른거
+                if(left.compareTo(right) < 0){ // 음수면 사전순이 left -> right
+                    answer+= left;
+                }else answer+= right; 
             }
-            
         }
+        
         return answer;
     }
 }
