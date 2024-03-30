17f4d6cb009f64fb289ceb999abf657194c2ad4e
Doyeon04 <71643491+Doyeon04@users.noreply.github.com>
2024-03-30 21:25:51 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.17 ms, Memory: 68.7 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -1,17 +1,16 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
-        int minCol = 51, minRow = 51, maxCol = 0, maxRow = 0; 
+        int minRow=50, minCol=50, maxRow=0, maxCol=0; 
         
         for(int i=0; i<wallpaper.length; i++){
             for(int j=0; j<wallpaper[i].length(); j++){
-                if(wallpaper[i].charAt(j)=='.') continue;
+                if(wallpaper[i].charAt(j)=='.') continue; 
                 minRow = Math.min(minRow, i);
                 minCol = Math.min(minCol, j);
                 maxRow = Math.max(maxRow, i);
                 maxCol = Math.max(maxCol, j); 
             }
         }
-        
         return new int[]{minRow, minCol, maxRow+1, maxCol+1};
     }
 }
